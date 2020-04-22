87bb88d26f53f7a1d2f3edb5d2963b368cbac37a
SSen <i@ssen.name>
2020-04-22 16:27:07 +0900

    feat: improve electron auto update

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,6 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
+import { RESTART_CHANNEL, UPDATE_CHANNEL } from './update';
 
 let window = null;
 
@@ -20,7 +21,7 @@ function createWindow() {
     });
   }
 
-  window.on('closed', function() {
+  window.on('closed', function () {
     window = null;
   });
 
@@ -28,19 +29,54 @@ function createWindow() {
 }
 
 app.on('ready', () => {
-  autoUpdater.checkForUpdatesAndNotify().finally(() => {
-    createWindow();
+  createWindow();
+
+  autoUpdater.autoDownload = false;
+  autoUpdater.autoInstallOnAppQuit = true;
+
+  window.webContents.on('did-finish-load', () => {
+    window.webContents.send(UPDATE_CHANNEL, { type: 'start-update-check' });
+
+    autoUpdater
+      .checkForUpdates()
+      .then((result) => {
+        window.webContents.send(UPDATE_CHANNEL, { type: 'update-checked', result });
+
+        if (result && result.updateInfo.files.length > 0) {
+          window.webContents.send(UPDATE_CHANNEL, { type: 'update-download-start' });
+
+          autoUpdater.downloadUpdate().then(() => {
+            window.webContents.send(UPDATE_CHANNEL, { type: 'update-ready' });
+          });
+        }
+      })
+      .catch((error) => {
+        window.webContents.send(UPDATE_CHANNEL, { type: 'update-error', error });
+      });
+
+    autoUpdater.addListener('download-progress', ({ percent }) => {
+      window.webContents.send(UPDATE_CHANNEL, {
+        type: 'update-download-progress',
+        progress: percent / 100,
+      });
+    });
+
+    autoUpdater.addListener('update-downloaded', (info) => {
+      window.webContents.send(UPDATE_CHANNEL, { type: 'update-ready' });
+    });
   });
 });
 
-app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+  app.quit();
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
   if (!window) {
     createWindow();
   }
+});
+
+ipcMain.on(RESTART_CHANNEL, () => {
+  autoUpdater.quitAndInstall(false, true);
 });
---
+++ src/app/update.js
@@ -0,0 +1,2 @@
+export const UPDATE_CHANNEL = 'app-update';
+export const RESTART_CHANNEL = 'app-restart';
